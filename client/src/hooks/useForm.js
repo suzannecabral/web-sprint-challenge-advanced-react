08c8ef4059d494f9eb1f72b2425fc8c797f69e53
Suzanne Cabral <suzannecabral@live.com>
2020-10-02 10:31:17 -0700

    Stuck on custom hooks, saving progress

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,37 @@
+import React from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
 // write your custom hook here to control your checkout form
+
+export const useForm = (key, initialValues) => {
+
+    const [formValues, setFormValues] = useLocalStorage(key, initialValues);
+
+    // const handleChanges = e = {
+    //     setFormValues({
+    //         ...formValues,
+    //         [e.target.name]:e.target.value
+    //     });
+    // };
+
+        const handleChanges = e = {
+        setFormValues({
+            [e.target.name],e.target.value
+        });
+    }
+
+    //this is for the form buttonss
+
+    //submit button, also submit on enter key
+    const handleSubmit = e => {
+        e.preventDefault();
+    };
+
+    //clear button
+    const clearForm = e => {
+        e.preventDefault();
+        setFormValues(initialValues);
+    };
+
+    return[formValues, clearForm, handleSubmit, handleChanges];
+};
